5174d18c1baabb59d9d8e8b8fa8731e15b4a76b5
nikitashrestha <nikshres522@gmail.com>
2020-08-14 17:14:52 +0545

    minor-fixes: Added method to predict the cancer label using random model: Log Loss: 2.45 on test and 2.48 on cross-validate data

--- main.py
+++ main.py
@@ -206,6 +206,102 @@ def show_distribution(y_train, y_test, y_cv):
         )
 
 
+# This function plots the confusion matrices given y_i, y_i_hat.
+def plot_confusion_matrix(test_y, predict_y):
+    C = confusion_matrix(test_y, predict_y)
+    # C = 9,9 matrix, each cell (i,j) represents number of points of class i are predicted class j
+
+    A = ((C.T) / (C.sum(axis=1))).T
+    # divid each element of the confusion matrix with the sum of elements in that column
+
+    # C = [[1, 2],
+    #     [3, 4]]
+    # C.T = [[1, 3],
+    #        [2, 4]]
+    # C.sum(axis = 1)  axis=0 corresonds to columns and axis=1 corresponds to rows in two diamensional array
+    # C.sum(axix =1) = [[3, 7]]
+    # ((C.T)/(C.sum(axis=1))) = [[1/3, 3/7]
+    #                           [2/3, 4/7]]
+
+    # ((C.T)/(C.sum(axis=1))).T = [[1/3, 2/3]
+    #                           [3/7, 4/7]]
+    # sum of row elements = 1
+
+    B = C / C.sum(axis=0)
+    # divid each element of the confusion matrix with the sum of elements in that row
+    # C = [[1, 2],
+    #     [3, 4]]
+    # C.sum(axis = 0)  axis=0 corresonds to columns and axis=1 corresponds to rows in two diamensional array
+    # C.sum(axix =0) = [[4, 6]]
+    # (C/C.sum(axis=0)) = [[1/4, 2/6],
+    #                      [3/4, 4/6]]
+
+    labels = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # representing A in heatmap format
+    print("-" * 20, "Confusion matrix", "-" * 20)
+    plt.figure(figsize=(20, 7))
+    sns.heatmap(
+        C, annot=True, cmap="YlGnBu", fmt=".3f", xticklabels=labels, yticklabels=labels
+    )
+    plt.xlabel("Predicted Class")
+    plt.ylabel("Original Class")
+    plt.show()
+
+    print("-" * 20, "Precision matrix (Columm Sum=1)", "-" * 20)
+    plt.figure(figsize=(20, 7))
+    sns.heatmap(
+        B, annot=True, cmap="YlGnBu", fmt=".3f", xticklabels=labels, yticklabels=labels
+    )
+    plt.xlabel("Predicted Class")
+    plt.ylabel("Original Class")
+    plt.show()
+
+    # representing B in heatmap format
+    print("-" * 20, "Recall matrix (Row sum=1)", "-" * 20)
+    plt.figure(figsize=(20, 7))
+    sns.heatmap(
+        A, annot=True, cmap="YlGnBu", fmt=".3f", xticklabels=labels, yticklabels=labels
+    )
+    plt.xlabel("Predicted Class")
+    plt.ylabel("Original Class")
+    plt.show()
+
+
+def predict_using_random_model(x_test, y_test, x_cv, y_cv):
+    # We need to generate 9 numbers and the sum of numbers should be 1
+    # one solution is to genarate 9 numbers and divide each of the numbers by their sum
+    # ref: https://stackoverflow.com/a/18662466/4084039
+    test_data_len = x_test.shape[0]
+    cv_data_len = x_cv.shape[0]
+
+    # we create a output array that has exactly same size as the CV data
+    cv_predicted_y = np.zeros((cv_data_len, 9))
+    for i in range(cv_data_len):
+        rand_probs = np.random.rand(1, 9)
+        cv_predicted_y[i] = (rand_probs / sum(sum(rand_probs)))[0]
+    print(
+        "Log loss on Cross Validation Data using Random Model",
+        log_loss(y_cv, cv_predicted_y, eps=1e-15),
+    )
+
+    # Test-Set error.
+    # We create a output array that has exactly same as the test data
+    test_predicted_y = np.zeros((test_data_len, 9))
+    for i in range(test_data_len):
+        rand_probs = np.random.rand(1, 9)
+        test_predicted_y[i] = (rand_probs / sum(sum(rand_probs)))[0]
+    print(
+        "Log loss on Test Data using Random Model",
+        log_loss(y_test, test_predicted_y, eps=1e-15),
+    )
+
+    predicted_y = np.argmax(test_predicted_y, axis=1)
+
+    plot_confusion_matrix(y_test, predicted_y + 1)
+
+    return predicted_y, y_test
+
+
 if __name__ == "__main__":
     # Load stopwords from nltk library
     nltk.download("stopwords")
@@ -239,3 +335,6 @@ if __name__ == "__main__":
 
     # View distribution of train, test and cross-validate
     show_distribution(y_train, y_test, y_cv)
+
+    # Predict the class using a random Model
+    predict_using_random_model(x_test, y_test, x_cv, y_cv)
